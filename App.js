9ce3274cfe80561b6c231666d64b4eb13ad7afa1
Shakil Khan <shakilsak05@gmail.com>
2023-05-03 13:35:37 +0500

    initial commit of app code

--- App.js
+++ App.js
@@ -3,10 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.red}>Welcome to</Text>
+        <Text style={styles.bigBlue}>SAAMMAN.COM</Text>
+        <Text style={[styles.bigBlue, styles.red]}>Developed by</Text>
+        <Text style={[styles.red]}>SHAKIL AHMAD KHAN</Text>
+
+        <View
+            style={{
+              width: 100,
+              height: 100,
+              borderRadius: 202,
+              backgroundColor: 'skyblue',
+            }}
+        />
+
+      </View>
   );
 }
 
@@ -16,5 +28,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  red: {
+    color: 'red'
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
 });
